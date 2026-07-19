0f3ccaaced6bfdb860d7aadf92fdd4d2da19de24
SolabSense <islomtrade11@gmail.com>
2026-07-19 12:01:18 +0500

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,90 @@
+TEXTS = {
+    "welcome": {
+        "ru": "⭐ Добро пожаловать!",
+        "uz": "⭐ Xush kelibsiz!"
+    },
+    "choose_stars": {
+        "ru": "⭐️ Сколько звёзд вы хотите купить?",
+        "uz": "⭐️ Nechta yulduz sotib olmoqchisiz?"
+    },
+    "price": {
+        "ru": "💰 Цена",
+        "uz": "💰 Narx"
+    },
+    "min": {
+        "ru": "🔹 Минимум: 50 звёзд",
+        "uz": "🔹 Minimum: 50 yulduz"
+    },
+    "use_buttons": {
+        "ru": "👇 Используйте кнопки ниже:",
+        "uz": "👇 Quyidagi tugmalardan foydalaning:"
+    },
+    "balance": {
+        "ru": "💰 Ваш баланс",
+        "uz": "💰 Sizning balansingiz"
+    },
+    "deposit": {
+        "ru": "💳 Пополнение\nВведите сумму (мин 1000 UZS):",
+        "uz": "💳 Hisobni to‘ldirish\nSummani kiriting (min 1000 UZS):"
+    },
+    "history_empty": {
+        "ru": "📭 История пуста",
+        "uz": "📭 Tarix bo‘sh"
+    },
+    "history": {
+        "ru": " История заказов",
+        "uz": " Buyurtmalar tarixi"
+    },
+    "choose_lang": {
+        "ru": "🌐 Выберите язык:",
+        "uz": "🌐 Tilni tanlang:"
+    },
+    "lang_changed": {
+        "ru": "✅ Язык изменён",
+        "uz": "✅ Til o‘zgartirildi"
+    },
+    "main_menu": {
+        "ru": "🏠 Главное меню",
+        "uz": "🏠 Asosiy menyu"
+    },
+    "not_enough": {
+        "ru": "❌ Недостаточно средств",
+        "uz": "❌ Mablag‘ yetarli emas"
+    },
+    "processing": {
+        "ru": "⏳ Обрабатываем заказ... Пожалуйста подождите",
+        "uz": "⏳ Buyurtma bajarilmoqda... Iltimos kuting"
+    },
+    "order_success": {
+        "ru": "🌟 Заказ успешно выполнен!",
+        "uz": "🌟 Buyurtma muvaffaqiyatli bajarildi!"
+    },
+    "error_order": {
+        "ru": "❌ Ошибка при выполнении заказа",
+        "uz": "❌ Buyurtmada xatolik yuz berdi"
+    },
+    "send_admin": {
+        "ru": "💬 Напишите нашему админу",
+        "uz": "💬 Admin bilan bog‘laning"
+    },
+    "min_deposit": {
+        "ru": "❌ Минимум 1000 UZS",
+        "uz": "❌ Minimum 1000 UZS"
+    },
+    "send_screenshot": {
+        "ru": "❌ Отправьте скриншот",
+        "uz": "❌ Skrinshot yuboring"
+    },
+    "enter_number": {
+        "ru": "❌ Введите число",
+        "uz": "❌ Raqam kiriting"
+    },
+    "check_sent": {
+        "ru": "✅ Чек отправлен на проверку",
+        "uz": "✅ Chek tekshiruvga yuborildi"
+    },
+    "instruction": {
+        "ru": "📖 Инструкция",
+        "uz": "📖 Yo'riqnoma"
+    }
+}
